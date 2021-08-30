9a3608bb4467434bb3c42900c7fc86c8a83d19e6
RoaaMustafa <labushanab14@gmail.com>
2021-08-30 16:10:39 +0300

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Summary
+
+# Django Models
+
+## Summary
+
+This is Introduction project for Django used to educational Purpose.
+
+**Stages**
+
++ Django project with data persistence
+
++ Create inter-related models
+
++ Use Django's Generic views to handle displaying Models
+
+## cooperation
+
+I work with Abdullah Elian And Anas AbuGhaliah
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class snacksTests(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_templete(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
